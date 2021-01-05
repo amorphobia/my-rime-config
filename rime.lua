bd329622757838d667a205a7e4d2b985985b5c12
amorphobia <pengxuesong.cn@gmail.com>
2021-01-05 17:38:34 +0800

    将配方的 lua 函数放到 rime.lua 以外，使得其他配方也可使用各自的 lua 函数

---
+++ rime.lua
@@ -0,0 +1,7 @@
+-- Openfly lua engines
+--   1. translators
+openfly_date_translator = require("openfly_date_translator")
+openfly_time_translator = require("openfly_time_translator")
+
+--   2. filters
+openfly_hint_filter = require("openfly_hint_filter")
