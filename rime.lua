6068b71494641c0a752416d004cf904b648d7321
amorphobia <pengxuesong.cn@gmail.com>
2021-01-18 15:00:27 +0800

    openfly 实现部分直通车命令

--- rime.lua
+++ rime.lua
@@ -1,7 +1,11 @@
 -- Openfly lua engines
---   1. translators
+--   1. processors
+openfly_shortcut_processor = require("openfly_shortcut_processor")
+
+--   2. segmentors
+--   3. translators
 openfly_date_translator = require("openfly_date_translator")
 openfly_time_translator = require("openfly_time_translator")
 
---   2. filters
+--   4. filters
 openfly_hint_filter = require("openfly_hint_filter")
