b06e93503809c84e97259258297274962c8cf81a
amorphobia <pengxuesong.cn@gmail.com>
2021-12-31 11:14:50 +0800

    rime.lua 中删除 openfly 脚本

--- rime.lua
+++ rime.lua
@@ -1,18 +1,3 @@
--- Openfly lua engines
---   1. processors
-openfly_shortcut_processor = require("openfly_shortcut_processor")
-
---   2. segmentors
---   3. translators
-openfly_date_translator = require("openfly_date_translator")
-openfly_time_translator = require("openfly_time_translator")
-openfly_shortcut_translator = require("openfly_shortcut_translator")
-
---   4. filters
-openfly_hint_filter = require("openfly_hint_filter")
-openfly_deletion_filter = require("openfly_deletion_filter")
-
-
 -- XKJD6 lua engines
 --[[
 librime-lua 样例
