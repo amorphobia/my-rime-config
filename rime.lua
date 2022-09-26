a7a5fddc33ca6488827b9d4a3c028533bc8e2f11
amorphobia <pengxuesong.cn@gmail.com>
2022-09-26 17:05:04 +0800

    调整 schema

--- rime.lua
+++ rime.lua
@@ -2,9 +2,5 @@
 date_time_translator = require("date_time")
 xkjd6_filter = require("xkjd6_filter")
 
--- XKJD6 user lua engines
-split = require("split")
-del = require("del")
-
 -- quad lua engines
 quad_filter = require("quad_filter")
