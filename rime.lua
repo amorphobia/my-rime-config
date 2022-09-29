2dc06da62978dccc086c08ac9a90a303ec6f0e6a
amorphobia <pengxuesong.cn@gmail.com>
2022-09-29 20:09:42 +0800

    调整 custom, 将 split 加回来

--- rime.lua
+++ rime.lua
@@ -2,5 +2,8 @@
 date_time_translator = require("date_time")
 xkjd6_filter = require("xkjd6_filter")
 
+-- XKJD6 user lua engines
+split = require("split")
+
 -- quad lua engines
 quad_filter = require("quad_filter")
